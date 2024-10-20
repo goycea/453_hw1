cd5cea9a2a47b8ab6c9007fb2c025b67eb5c3059
Alperen Göyce <goycea@gmail.com>
2024-10-20 15:47:47 +0300

    Update ShoppingCartTest.java

--- src/ShoppingCartTest.java
+++ src/ShoppingCartTest.java
@@ -1,39 +1,103 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
 
+    private ShoppingCart cart;
+    private ProductModel product1;
+    private ProductModel product2;
+
+    @BeforeEach
+    void setUp() {
+        cart = new ShoppingCart();
+        product1 = new ProductModel("Apple", 1, 1, "kg");
+        product2 = new ProductModel("Banana", 2, 1, "kg");
+    }
+
+    @Test
+    void getProducts() {
+        cart.addProduct(product1, 1);
+        ProductModel[] products = cart.getProducts();
+        assertEquals(1, products[0].getQuantity());
+        assertEquals("Apple", products[0].getName());
+    }
+
     @Test
     void addProduct() {
-        ShoppingCart cart = new ShoppingCart();
-        ProductModel product = new ProductModel("TestProduct", 100, 10, "kg");
-        int quantity = 4;
-        cart.addProduct(product,quantity);
-        assertEquals(100, cart.getTotalCost());
+        cart.addProduct(product1, 1);
         assertEquals(1, cart.getTotalProducts());
+        assertEquals(1, cart.getProducts()[0].getQuantity());
+        assertEquals(1, cart.getTotalCost());
+
+        cart.addProduct(product1, 2);
+        assertEquals(1, cart.getTotalProducts());
+        assertEquals(3, cart.getProducts()[0].getQuantity());
+        assertEquals(3, cart.getTotalCost());
+
+        cart.addProduct(product2, 1);
+        assertEquals(2, cart.getTotalProducts());
+        assertEquals(1, cart.getProducts()[1].getQuantity());
+        assertEquals(5, cart.getTotalCost());
     }
 
     @Test
     void removeProduct() {
-        ShoppingCart cart = new ShoppingCart();
-        ProductModel product = new ProductModel("TestProduct", 100, 10, "piece");
-        int quantity = 4;
-        cart.addProduct(product,quantity);
-        cart.removeProduct(product, 10);
-        assertEquals(0, cart.getTotalCost());
+        cart.addProduct(product1, 3);
+        cart.addProduct(product2, 2);
+
+        int removedQuantity = cart.removeProduct(product1, 1);
+        assertEquals(1, removedQuantity);
+        assertEquals(2, cart.getProducts()[0].getQuantity());
+        assertEquals(6, cart.getTotalCost());
+
+        removedQuantity = cart.removeProduct(product1, 2);
+        assertEquals(2, removedQuantity);
+        assertEquals(1, cart.getTotalProducts());
+        assertEquals(4, cart.getTotalCost());
+
+        removedQuantity = cart.removeProduct(product2, 2);
+        assertEquals(2, removedQuantity);
         assertEquals(0, cart.getTotalProducts());
+        assertEquals(4, cart.getTotalCost());
+    }
+
+    @Test
+    void getTotalCost() {
+        cart.addProduct(product1, 1);
+        cart.addProduct(product2, 2);
+        assertEquals(5, cart.getTotalCost());
+    }
+
+    @Test
+    void getTotalProducts() {
+        cart.addProduct(product1, 1);
+        cart.addProduct(product2, 2);
+        assertEquals(2, cart.getTotalProducts());
+    }
+
+    @Test
+    void findProductIndex() {
+        cart.addProduct(product1, 1);
+        cart.addProduct(product2, 2);
+        assertEquals(0, cart.findProductIndex(product1));
+        assertEquals(1, cart.findProductIndex(product2));
     }
 
     @Test
     void printCart() {
-        ShoppingCart cart = new ShoppingCart();
-        ProductModel product = new ProductModel("TestProduct", 100, 10, "liter");
-        int quantity = 4;
-        cart.addProduct(product,quantity);
+        cart.addProduct(product1, 1);
+        cart.addProduct(product2, 2);
         cart.printCart();
-        assertEquals(1000, cart.getTotalCost());
-        assertEquals(1, cart.getTotalProducts());
+    }
 
+    @Test
+    void clearCart() {
+        cart.addProduct(product1, 1);
+        cart.addProduct(product2, 2);
+        cart.clearCart();
+        assertEquals(0, cart.getTotalProducts());
+        assertEquals(0, cart.getTotalCost());
     }
 }
